f76681327f3b87473b28655ebebef1545b8b12d6
rexno7 <rexno7@gmail.com>
2020-08-30 13:47:55 -0500

    refactor draw.js to use start button

--- draw.js
+++ draw.js
@@ -1,65 +1,62 @@
-const canvas = document.querySelector(".canvas");
-const ctx = canvas.getContext("2d");
-const scale = 10;
-const rows = canvas.height / scale;
-const columns = canvas.width / scale;
-const startBtn = document.getElementById("startBtn")
+// milliseconds per frame (speed of snek)
 let speed = 200;
 
-let snek;
+// canvas
+let ctx;
+let rows;
+let columns;
+let scale;
 
-(function setup() {
+// sprites
+let snek;
+let fruit;
+
+// frame interval
+let interval;
+
+const initialize = () => {
+  console.log("init");
+  // init canvas
+  var canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  scale = 10;
+  rows = canvas.height / scale;
+  columns = canvas.width / scale;
+
+  // init sprites
   snek = new Snek(2*scale, 2*scale);
   fruit = new Fruit();
-
   fruit.pickLocation();
 
-  let id = window.setInterval(frame, speed);
+  // paint canvas
+  paint();
 
-  function frame() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    fruit.draw();
-    snek.update();
-    if (snek.checkCollision()) {
-      alert("GAME OVER");
-      document.location.reload();
-      clearInterval(id);
-    } else {
-      snek.draw();
-    }
+  // init frame
+  interval = window.setInterval(paint, speed);
+};
 
-    if (snek.eat(fruit)) {
-      fruit.pickLocation();
-    }
+const paint = () => {
+  // paint the scene
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  fruit.draw();
+  snek.update();
 
-    if (snek.tailLength === 0) {
-      speed = 200;
-      clearInterval(id);
-      id = window.setInterval(frame, speed);
-    } else if (snek.tailLength === 10) {
-      speed = 150;
-      clearInterval(id);
-      id = window.setInterval(frame, speed);
-    } else if (snek.tailLength === 20) {
-      speed = 110;
-      clearInterval(id);
-      id = window.setInterval(frame, speed);
-    } else if (snek.tailLength === 30) {
-      speed = 80;
-      clearInterval(id);
-      id = window.setInterval(frame, speed);
-    } else if (snek.tailLength === 40) {
-      speed = 55;
-      clearInterval(id);
-      id = window.setInterval(frame, speed);
-    } else if (snek.tailLength === 50) {
-      speed = 40;
-      clearInterval(id);
-      id = window.setInterval(frame, speed);
-    }
-  };
-}());
+  // check for collision and draw snek
+  if (snek.checkCollision()) {
+    alert("GAME OVER");
+    document.location.reload();
+    clearInterval(interval);
+  } else {
+    snek.draw();
+  }
 
+  // check for fruit collision
+  if (snek.eat(fruit)) {
+    fruit.pickLocation();
+  }
+}
+
+// read user input
 window.addEventListener('keydown', ((event) => {
   const direction = event.key.replace('Arrow', '');
   snek.changeDirection(direction);
