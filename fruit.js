ee09f0c83093f93a7da288c2ccf3307e6589190c
rexno7 <rexno7@gmail.com>
2020-09-19 02:05:58 -0500

    use apple image instead of drawing

--- fruit.js
+++ fruit.js
@@ -23,26 +23,29 @@ function Fruit() {
   };
 
   this.draw = () => {
-    const radius = scale / 2;
+    ctx.drawImage(apple, this.x, this.y, scale, scale);
+
+
+    // const radius = scale / 2;
 
     // apple
-    ctx.beginPath();
-    ctx.arc(this.x + radius, this.y + radius, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = "red";
-    ctx.fill();
+    // ctx.beginPath();
+    // ctx.arc(this.x + radius, this.y + radius, radius, 0, 2 * Math.PI);
+    // ctx.fillStyle = "red";
+    // ctx.fill();
 
     // stem
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.moveTo(this.x + radius, this.y + (radius / 2));
-    ctx.lineTo(this.x + radius, this.y - (radius / 4));
-    ctx.strokeStyle = "tan";
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.lineWidth = 1;
+    // ctx.moveTo(this.x + radius, this.y + (radius / 2));
+    // ctx.lineTo(this.x + radius, this.y - (radius / 4));
+    // ctx.strokeStyle = "tan";
+    // ctx.stroke();
 
     // leaf
-    ctx.beginPath();
-    ctx.ellipse(this.x + scale - (radius / 2), this.y, radius / 2, radius / 4, 0, 0, 2 * Math.PI);
-    ctx.fillStyle = "limegreen";
-    ctx.fill();
+    // ctx.beginPath();
+    // ctx.ellipse(this.x + scale - (radius / 2), this.y, radius / 2, radius / 4, 0, 0, 2 * Math.PI);
+    // ctx.fillStyle = "limegreen";
+    // ctx.fill();
   }
 }
