00b061d2d40d4133d6f23b50dd516df94dda1761
rexno7 <rexno7@gmail.com>
2020-09-19 18:37:34 -0500

    update apple to cleaner image

--- fruit.js
+++ fruit.js
@@ -24,28 +24,5 @@ function Fruit() {
 
   this.draw = () => {
     ctx.drawImage(apple, this.x, this.y, scale, scale);
-
-
-    // const radius = scale / 2;
-
-    // apple
-    // ctx.beginPath();
-    // ctx.arc(this.x + radius, this.y + radius, radius, 0, 2 * Math.PI);
-    // ctx.fillStyle = "red";
-    // ctx.fill();
-
-    // stem
-    // ctx.beginPath();
-    // ctx.lineWidth = 1;
-    // ctx.moveTo(this.x + radius, this.y + (radius / 2));
-    // ctx.lineTo(this.x + radius, this.y - (radius / 4));
-    // ctx.strokeStyle = "tan";
-    // ctx.stroke();
-
-    // leaf
-    // ctx.beginPath();
-    // ctx.ellipse(this.x + scale - (radius / 2), this.y, radius / 2, radius / 4, 0, 0, 2 * Math.PI);
-    // ctx.fillStyle = "limegreen";
-    // ctx.fill();
   }
 }
